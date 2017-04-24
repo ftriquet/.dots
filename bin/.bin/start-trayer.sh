41362be900efcac2802c207a7dac4cf9c7886eb8
François Triquet <francois.triquet@recast.ai>
2017-04-24 23:42:07 +0200

    fix trayer script

--- bin/.bin/start-trayer.sh
+++ bin/.bin/start-trayer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-[[ $(pgrep trayer) ]] && exit 0
+killall trayer
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 5 --height 16 --transparent true --widthtype percent --tint 0x1D2021 --alpha 0
